51e2a7ba8bcef4b03c07b097b38a8ff78003eefa
Stanley <stanleyemeke71@gmail.com>
2024-05-21 18:51:26 -0700

    Javascript - web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,56 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-const id = process.argv[2];
-
-if (!id) {
-  console.error('Usage: ./script.js <film_id>');
-  process.exit(1);
-}
-
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching film:', error);
-    return;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch film. Status code: ${response.statusCode}`);
-    return;
-  }
-
-  let content;
-  try {
-    content = JSON.parse(body);
-  } catch (e) {
-    console.error('Error parsing JSON:', e);
-    return;
-  }
-
-  const characters = content.characters;
-  // console.log(characters);
-  characters.forEach(character => {
-    request.get(character, (error, response, body) => {
-      if (error) {
-        console.error('Error fetching character:', error);
-        return;
-      }
-      if (response.statusCode !== 200) {
-        console.error(`Failed to fetch character. Status code: ${response.statusCode}`);
-        return;
-      }
-
-      let characterData;
-      try {
-        characterData = JSON.parse(body);
-      } catch (e) {
-        console.error('Error parsing character JSON:', e);
-        return;
-      }
-
-      console.log(characterData.name);
-    });
-  });
 });
 
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
